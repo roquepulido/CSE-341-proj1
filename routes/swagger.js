52a990ab4bc9c46274c444edc00d51276b1e63d8
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2025-05-17 23:40:41 -0600

    ADD import json file

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const swaggerRouter = Router(); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// read the swagger.json file
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../swagger.json"), "utf-8")
+);
+
+const swaggerRouter = Router();
 
 swaggerRouter.use("/api-docs", swaggerUi.serve);
 swaggerRouter.get("/api-docs", swaggerUi.setup(swaggerDocument));
